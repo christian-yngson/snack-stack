62cd719abed4b37ac8ee68589e1a0cab89478885
Christian Yngson <christianwillyngson@gmail.com>
2026-01-03 00:29:27 +0800

    feat: add products filter

---
+++ src/components/products/MainProducts/ProductsFilter/ProductsFilter.tsx
@@ -0,0 +1,21 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import HeadingAndFIlterReset from "./HeadingAndFIlterReset";
+import ProductsFilterElements from "./ProductsFilterElements";
+import Stack from "@mui/material/Stack";
+
+/* @TODO add test immediate */
+function ProductsFilter() {
+  return (
+    <Card sx={{ minWidth: 275 }} variant="outlined">
+      <CardContent>
+        <Stack gap={2}>
+          <HeadingAndFIlterReset />
+          <ProductsFilterElements />
+        </Stack>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default ProductsFilter;
