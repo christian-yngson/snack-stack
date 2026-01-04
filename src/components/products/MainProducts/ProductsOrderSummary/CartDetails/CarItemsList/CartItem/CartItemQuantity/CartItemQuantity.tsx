6654097332c38ebbbe75bb1b580fc79f1e9a6695
Christian Yngson <christianwillyngson@gmail.com>
2026-01-05 01:40:25 +0800

    feat: connect cart item quantity to the redux store

--- src/components/products/MainProducts/ProductsOrderSummary/CartDetails/CarItemsList/CartItem/CartItemQuantity/CartItemQuantity.tsx
+++ src/components/products/MainProducts/ProductsOrderSummary/CartDetails/CarItemsList/CartItem/CartItemQuantity/CartItemQuantity.tsx
@@ -3,9 +3,38 @@ import RemoveIcon from "@mui/icons-material/Remove";
 import AddIcon from "@mui/icons-material/Add";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
+import { connect } from "react-redux";
+import Tooltip from "@mui/material/Tooltip";
+import {
+  increaseQuantity,
+  decreaseQuantity,
+} from "@/redux/features/cart/cartSlice";
+import { type Dispatch } from "@reduxjs/toolkit";
+import { type RootState } from "@/redux/store";
+import Box from "@mui/material/Box";
+
+interface OwnProps {
+  id: string | number;
+}
+
+interface StateProps {
+  quantity: number;
+}
+
+interface DispatchProps {
+  increaseQuantity: (id: string | number) => void;
+  decreaseQuantity: (id: string | number) => void;
+}
+
+type Props = OwnProps & StateProps & DispatchProps;
 
 /* @TODO add test immediate */
-function CartItemQuantity() {
+function CartItemQuantity({
+  id,
+  quantity,
+  increaseQuantity,
+  decreaseQuantity,
+}: Props) {
   return (
     <Stack
       direction="row"
@@ -19,15 +48,55 @@ function CartItemQuantity() {
       }}
       alignSelf="start"
     >
-      <IconButton size="small">
-        <RemoveIcon fontSize="small" />
-      </IconButton>
-      <Typography variant="body2">1</Typography>
-      <IconButton size="small">
+      <Tooltip title={quantity <= 1 ? "Quantity cannot go below 1" : ""}>
+        <Box component="span">
+          <IconButton
+            size="small"
+            onClick={() => decreaseQuantity(id)}
+            disabled={quantity <= 1}
+          >
+            <RemoveIcon fontSize="small" />
+          </IconButton>
+        </Box>
+      </Tooltip>
+      <Typography variant="body2">{quantity}</Typography>
+      <IconButton
+        size="small"
+        onClick={() => increaseQuantity(id)}
+        disabled={quantity >= 10}
+      >
         <AddIcon fontSize="small" />
       </IconButton>
     </Stack>
   );
 }
 
-export default CartItemQuantity;
+/* 
+  Hooks are the modern and recommended way to connect to the redux store.
+  The implementation below using connect is for demonstrating legacy Redux knowledge.
+*/
+function mapStateToProps(state: RootState, ownProps: OwnProps): StateProps {
+  return {
+    quantity:
+      state.cart.value.find((item) => item.id === ownProps.id)?.quantity ?? 0,
+  };
+}
+
+function mapDispatchToProps(dispatch: Dispatch): DispatchProps {
+  return {
+    increaseQuantity: (id: string | number) => dispatch(increaseQuantity(id)),
+    decreaseQuantity: (id: string | number) => dispatch(decreaseQuantity(id)),
+  };
+}
+
+const ConnectedCartItemQuantity = connect<
+  StateProps,
+  DispatchProps,
+  OwnProps,
+  RootState
+>(
+  mapStateToProps,
+  mapDispatchToProps
+)(CartItemQuantity);
+
+export default ConnectedCartItemQuantity;
