4ed7b156112bc3b122d325b50577e8728d8b90ad
Christian Yngson <christianwillyngson@gmail.com>
2026-01-05 01:26:34 +0800

    feat: add cart items list

---
+++ src/components/products/MainProducts/ProductsOrderSummary/CartDetails/CarItemsList/CarItemsList.tsx
@@ -0,0 +1,10 @@
+import CartItem from "./CartItem";
+import { useSelector } from "react-redux";
+import { type RootState } from "@/redux/store";
+
+function CartItemsList() {
+  const cartItems = useSelector((state: RootState) => state.cart.value);
+  return cartItems.map((c) => <CartItem order={c} />);
+}
+
+export default CartItemsList;
