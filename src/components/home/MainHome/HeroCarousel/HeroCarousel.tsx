fe77ff504dc2190b8bf546bc6b1e8c6b9c0d3130
Christian Yngson <christianwillyngson@gmail.com>
2026-01-23 00:33:45 +0800

    feat: add hawaiian pizza image and carousel item reusable component

--- src/components/home/MainHome/HeroCarousel/HeroCarousel.tsx
+++ src/components/home/MainHome/HeroCarousel/HeroCarousel.tsx
@@ -1,12 +1,29 @@
 import Box from "@mui/material/Box";
 import CarouselItem from "./CarouselItem";
+import Burger from "@/assets/images/home/burger.jpg";
 
 /* @TODO add test immediate */
 
 function HeroCarousel() {
+  const carouselItems = [
+    {
+      image: Burger,
+      altText: "Burger",
+      title: "Welcome to Snack Stack",
+      subtitle: "Deliciousness delivered to your door",
+    },
+  ];
   return (
     <Box sx={{ position: "relative", width: "100%", height: "60vh" }}>
-      <CarouselItem />
+      {carouselItems.map((item) => (
+        <CarouselItem
+          key={item.image}
+          image={item.image}
+          altText={item.altText}
+          title={item.title}
+          subtitle={item.subtitle}
+        />
+      ))}
     </Box>
   );
 }
