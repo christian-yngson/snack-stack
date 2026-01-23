edfea6488e23f403542a83f74fd7639dbf139bfc
Christian Yngson <christianwillyngson@gmail.com>
2026-01-24 02:15:55 +0800

    feat: implement carousel next button

---
+++ src/components/home/MainHome/HeroCarousel/CarouselNextButton/CarouselNextButton.tsx
@@ -0,0 +1,28 @@
+import CarouselItemButton from "../CarouselItemButton";
+
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+
+interface Props {
+  onClick(): void;
+}
+
+function CarouselNextButton({ onClick }: Props) {
+  return (
+    <CarouselItemButton
+      aria-label="Next carousel item"
+      onClick={onClick}
+      sx={{
+        position: "absolute",
+        right: 20,
+        top: "50%",
+        transform: "translateY(-50%)",
+        zIndex: 1,
+        color: "white",
+      }}
+    >
+      <ArrowForwardIosIcon />
+    </CarouselItemButton>
+  );
+}
+
+export default CarouselNextButton;
