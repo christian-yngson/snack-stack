19b9b5bb08a61ac2817a4d2c1ec5f4cda775ba6a
Christian Yngson <christianwillyngson@gmail.com>
2026-01-24 02:16:49 +0800

    feat: initial caoursel layout

--- src/components/home/MainHome/HeroCarousel/CarouselItem/CarouselItem.tsx
+++ src/components/home/MainHome/HeroCarousel/CarouselItem/CarouselItem.tsx
@@ -2,7 +2,9 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
 import CarouselDarkOverlay from "../CarouselDarkOverlay";
-/* @TODO add test immediate */
+import { forwardRef } from "react";
+import MotionDiv from "@/components/common/MotionDiv";
+import Box from "@mui/material/Box";
 
 interface Props {
   image: string;
@@ -10,37 +12,82 @@ interface Props {
   title: string;
   subtitle: string;
 }
-function CarouselItem({ image, altText, title, subtitle }: Props) {
-  return (
-    <Stack sx={{ position: "relative" }}>
-      <CardMedia
-        component="img"
-        image={image}
-        alt={altText}
-        sx={{
-          width: "100%",
-          height: "60vh",
-          objectFit: "cover", // fills container, crops if needed
-        }}
-      />
-      <Stack
-        sx={{
-          position: "absolute",
-          top: "40%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          color: "white",
-          textAlign: "center",
-          zIndex: 1,
-        }}
-        gap={1}
-      >
-        <Typography variant="h1">{title}</Typography>
-        <Typography variant="h4">{subtitle}</Typography>
-      </Stack>
-      <CarouselDarkOverlay />
-    </Stack>
-  );
-}
+
+/* @TODO add test immediate */
+const CarouselItem = forwardRef<HTMLDivElement, Props>(
+  ({ image, altText, title, subtitle }, ref) => {
+    return (
+      <Box sx={{ position: "relative" }}>
+        <MotionDiv
+          ref={ref}
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { duration: 0.6, ease: "easeOut" },
+          }}
+          exit={{
+            opacity: 0,
+            transition: { duration: 0.4, ease: "easeIn" },
+          }}
+        >
+          <CardMedia
+            component="img"
+            image={image}
+            alt={altText}
+            sx={{
+              width: "100%",
+              height: "60vh",
+              objectFit: "cover", // fills container, crops if needed
+            }}
+          />
+          <Stack
+            className="typography"
+            sx={{
+              position: "absolute",
+              top: "40%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              color: "white",
+              textAlign: "center",
+              zIndex: 1,
+              width: "96%",
+            }}
+            gap={1}
+          >
+            <MotionDiv
+              initial={{ opacity: 0, x: -24 }}
+              animate={{
+                opacity: 1,
+                x: 0,
+                transition: {
+                  delay: 0.5,
+                  duration: 0.4,
+                  ease: "easeOut",
+                },
+              }}
+            >
+              <Typography variant="h1">{title}</Typography>
+            </MotionDiv>
+            <MotionDiv
+              initial={{ opacity: 0, x: 24 }}
+              animate={{
+                opacity: 1,
+                x: 0,
+                transition: {
+                  delay: 0.6,
+                  duration: 0.4,
+                  ease: "easeOut",
+                },
+              }}
+            >
+              <Typography variant="h4">{subtitle}</Typography>
+            </MotionDiv>
+          </Stack>
+          <CarouselDarkOverlay />
+        </MotionDiv>
+      </Box>
+    );
+  },
+);
 
 export default CarouselItem;
